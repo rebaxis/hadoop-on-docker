a7972c2fefcb585f6aa9084bfade6cb712f86bbc
16705009 <podrezenko.d.an@sberbank.ru>
2021-10-04 08:23:49 +0300

    change a lot

--- load_table.sh
+++ load_table.sh
@@ -5,5 +5,10 @@ while /opt/hive/bin/beeline -u jdbc:hive2://localhost:10000 -e "show databases;"
     sleep 10
 done
 
-/opt/hive/bin/beeline -u jdbc:hive2://localhost:10000 -e "CREATE TABLE pokes (foo INT, bar STRING); LOAD DATA LOCAL INPATH '/opt/hive/examples/files/kv1.txt' OVERWRITE INTO TABLE pokes;"
+while /opt/hadoop/bin/hdfs dfs -ls hdfs://namenode:8020/ ; ret=$? ; [ $ret -ne 0 ]; do
+    echo 'fail hdfs connection'
+    sleep 10
+done
+
+/opt/hive/bin/beeline -u jdbc:hive2://localhost:10000 -e "CREATE TABLE pokes (foo INT, bar STRING) LOCATION 'hdfs://namenode:8020/tmp/pokes';" -e "LOAD DATA LOCAL INPATH '/opt/hive/examples/files/kv1.txt' OVERWRITE INTO TABLE pokes;"
 
